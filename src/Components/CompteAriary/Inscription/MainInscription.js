a5909b938216f6465a84a1536620dde6e6098ec7
JeanSamuel <rjs.samuel@yahoo.com>
2017-09-01 18:16:31 +0300

    ddd

--- src/Components/CompteAriary/Inscription/MainInscription.js
+++ src/Components/CompteAriary/Inscription/MainInscription.js
@@ -1,76 +1,83 @@
 import React, { Component } from 'react';
-import { AppRegistry,StyleSheet,View, Text } from 'react-native';
+import { AppRegistry, StyleSheet, View, Text } from 'react-native';
 import ViewPager from 'react-native-viewpager';
 import StepIndicator from 'react-native-step-indicator';
 import EditMail from '../Config/EditMail';
-const PAGES = [<EditMail/>,<EditMail/>,<EditMail/>,<EditMail/>,<EditMail/>];
+const PAGES = [<EditMail />, <EditMail />, <EditMail />, <EditMail />, <EditMail />];
 
 const firstIndicatorStyles = {
-  stepIndicatorSize: 30,
-  currentStepIndicatorSize:40,
-  separatorStrokeWidth: 3,
-  currentStepStrokeWidth: 5,
-  separatorFinishedColor: '#4aae4f',
-  separatorUnFinishedColor: '#a4d4a5',
-  stepIndicatorFinishedColor: '#4aae4f',
-  stepIndicatorUnFinishedColor: '#a4d4a5',
-  stepIndicatorCurrentColor: '#ffffff',
-  stepIndicatorLabelFontSize: 15,
-  currentStepIndicatorLabelFontSize: 15,
-  stepIndicatorLabelCurrentColor: '#000000',
-  stepIndicatorLabelFinishedColor: '#ffffff',
-  stepIndicatorLabelUnFinishedColor: 'rgba(255,255,255,0.5)',
-  labelColor: '#666666',
-  labelSize: 12,
-  currentStepLabelColor: '#4aae4f'
-}
+	stepIndicatorSize: 30,
+	currentStepIndicatorSize: 40,
+	separatorStrokeWidth: 3,
+	currentStepStrokeWidth: 5,
+	separatorFinishedColor: '#4aae4f',
+	separatorUnFinishedColor: '#a4d4a5',
+	stepIndicatorFinishedColor: '#4aae4f',
+	stepIndicatorUnFinishedColor: '#a4d4a5',
+	stepIndicatorCurrentColor: '#ffffff',
+	stepIndicatorLabelFontSize: 15,
+	currentStepIndicatorLabelFontSize: 15,
+	stepIndicatorLabelCurrentColor: '#000000',
+	stepIndicatorLabelFinishedColor: '#ffffff',
+	stepIndicatorLabelUnFinishedColor: 'rgba(255,255,255,0.5)',
+	labelColor: '#666666',
+	labelSize: 12,
+	currentStepLabelColor: '#4aae4f'
+};
 
 export default class MainInscription extends Component {
+	constructor() {
+		super();
+		let dataSource = new ViewPager.DataSource({
+			pageHasChanged: (p1, p2) => p1 !== p2
+		});
+		this.state = {
+			dataSource: dataSource.cloneWithPages(PAGES),
+			currentPage: 0
+		};
+	}
 
-  constructor() {
-    super();
-    let dataSource = new ViewPager.DataSource({
-      pageHasChanged: (p1, p2) => p1 !== p2,
-    });
-    this.state = {
-      dataSource: dataSource.cloneWithPages(PAGES),
-      currentPage:0
-    }
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.stepIndicator}>
+					<StepIndicator
+						customStyles={firstIndicatorStyles}
+						currentPosition={this.state.currentPage}
+						labels={['Identité', 'Email', 'Sécurité', 'Confirmation', 'Dashboard']}
+					/>
+				</View>
+				<ViewPager
+					dataSource={this.state.dataSource}
+					renderPage={this.renderViewPagerPage}
+					onChangePage={page => {
+						this.setState({ currentPage: page });
+					}}
+				/>
+			</View>
+		);
+	}
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.stepIndicator}>
-          <StepIndicator customStyles={firstIndicatorStyles} currentPosition={this.state.currentPage} labels={["Identité","Email","Sécurité","Confirmation","Dashboard"]} />
-        </View>
-        <ViewPager
-          dataSource={this.state.dataSource}
-          renderPage={this.renderViewPagerPage}
-          onChangePage={(page) => {this.setState({currentPage:page})}}
-          />
-      </View>
-    );
-  }
-
-  renderViewPagerPage = (data) => {
-    return(<View style={styles.page}>
-      <Text>{data}</Text>
-    </View>)
-  }
+	renderViewPagerPage = data => {
+		return (
+			<View style={styles.page}>
+				<View>{data}</View>
+			</View>
+		);
+	};
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-  },
-  stepIndicator: {
-    marginVertical:50,
-  },
-  page: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#ffffff'
+	},
+	stepIndicator: {
+		marginVertical: 50
+	},
+	page: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
 });
